809aa94c074da477dcc52fca7df8b6fbdfed6e6a
rares018 <poparares100@gmail.com>
2026-08-14 22:49:03 +0300

    Update SAOW.py

--- SAOW.py
+++ SAOW.py
@@ -1,9 +1,10 @@
 import os
-
 print("==shell==")
 while True:
     cmd = input(">>>")
     try:
         os.system(cmd)
+    except KeyboardInterrupt:
+        pass
     except Exception as e:
         print(e)
